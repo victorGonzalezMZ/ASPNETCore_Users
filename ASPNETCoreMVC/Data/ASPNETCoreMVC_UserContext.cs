934a065285a6a140322a4ed0102a07e1ef0be3a8
Unknown <victor309710gonz@gmail.com>
2020-10-03 09:35:58 -0500

    Commit Upload

---
+++ ASPNETCoreMVC/Data/ASPNETCoreMVC_UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPNETCoreMVC.Models;
+
+namespace ASPNETCoreMVC.Data
+{
+    public class ASPNETCoreMVC_UserContext : DbContext
+    {
+        public ASPNETCoreMVC_UserContext (DbContextOptions<ASPNETCoreMVC_UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNETCoreMVC.Models.User> User { get; set; }
+    }
+}
